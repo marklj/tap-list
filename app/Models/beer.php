6b133495b70dc03cf6112ce72d950de745c01782
Mark Johnson <marklj@gmail.com>
2021-06-29 22:35:35 -0700

    add seed ref to model

--- app/Models/beer.php
+++ app/Models/beer.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
+use Database\Factories\BeerFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class beer extends Model
 {
     use HasFactory;
+
+    /** @return BeerFactory */
+    protected static function newFactory()
+    {
+        return BeerFactory::new();
+    }
 }
